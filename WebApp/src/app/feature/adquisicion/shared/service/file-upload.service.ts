b9adb61b786f2e16ddd9ba3702361063a7ca8811
klmeir <caalmeir@gmail.com>
2024-07-21 01:55:25 -0500

    replaces environment variables when building the docker image

--- WebApp/src/app/feature/adquisicion/shared/service/file-upload.service.ts
+++ WebApp/src/app/feature/adquisicion/shared/service/file-upload.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, delay, of, timeout } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { FileUploadResponseDto } from '../model/file-upload-response';
 
